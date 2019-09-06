82f93b474f94c5c7d64c736501a7582bcf25a425
lsat12357 <lsato@uoregon.edu>
2019-09-06 16:52:50 -0700

    add sample usage in comments

--- config/initializers/overrides/solr_documents_block_override.rb
+++ config/initializers/overrides/solr_documents_block_override.rb
@@ -1,8 +1,10 @@
 SirTrevorRails::Blocks::SolrDocumentsBlock.class_eval do
-  # enable use of custom exhibit field for alt text
+  # this is a helper for use of a custom exhibit field for alt text
   # use label Alt Text, which will result in solr field exhibit_exhibitname_alt-text_tesim
   # be sure to create it in the UI when exhibit is created
-  # TODO auto generate the field
+  # to use, perhaps something like:
+  # image_tag(block_options[:full_image_url], alt: doc_presenter.field_value(solr_documents_block.alt_text))
+  # TODO auto generate the field so it doesn't have to be created manually by the curator
   # This assembles the field name, since it changes from exhibit to exhibit
   def alt_text
     exhibit_id = solr_helper.current_exhibit.slug
